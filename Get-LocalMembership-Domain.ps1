53d13cb10d5ac5e527b6fa7cfe2df7f0cf478203
Blake Regan <39965542+crash0ver1d3@users.noreply.github.com>
2021-03-26 15:59:54 -0500

    Optimizing flexibility, adding more comments for better clarity
    -Added note to install RSAT AD Powershell module if not present on Windows Server 2012 R2 and above, command provided
    -Updated the method to identify the DN format of the domain container, to dynamically gather and provide to search base filter
    -Added timestamp variable to make unique report file names, with date and time for record keeping
    -Added more comments in script to identify actions taken, and explain logic

--- Get-LocalMembership-Domain.ps1
+++ Get-LocalMembership-Domain.ps1
@@ -10,7 +10,9 @@
 
 
 
-
+#requires Active Directory Module
+#If not installed, but you want to install the RSAT-AD Powershell tools, run the following command to install the built in windows feature, on WS2K12R2 and above
+#PS>Install-WindowsFeature RSAT-AD-PowerShell, no reboot needed.
 Import-Module ActiveDirectory
 $domain=(Get-WmiObject Win32_ComputerSystem).Domain.Split('.')
 
@@ -18,29 +20,30 @@ $domain=(Get-WmiObject Win32_ComputerSystem).Domain.Split('.')
 You can create a block for each OU that you want to monitor
 Plans to add detection for OUs that have computer objects and iterate through array
 #>
-if ($domain.Count -eq 1)
-{
-   $Servers=Get-ADComputer -Filter * -SearchBase "ou=Servers,dc=$($domain[0])" -Properties Name, DistinguishedName | Select-Object -Property Name,DistinguishedName 
-}
+#dc=contoso,dc=redmond,dc=local
+$root = [ADSI]"LDAP://RootDSE"
+#$DOMAIN = $root.Get("rootDomainNamingContext")
+$DOMAIN = $root.defaultNamingContext
 
-if ($domain.Count -eq 2)
-{
-   $Servers=Get-ADComputer -Filter * -SearchBase "ou=Servers,dc=$($domain[0]),dc=$($domain[1])" -Properties Name, DistinguishedName | Select-Object -Property Name,DistinguishedName 
-}
+#Gather query targets into an array, using the $DOMAIN variable to gather the Domain Container Format, like dc=contoso,dc=redmond,dc=local
+#You can target what every OU that you require, just fill in the Distinguished name in the search base "dc=contoso,dc=redmond,dc=local" format. $DOMAIN will
+#dynamically resolve to whatever domain you run this script in.
 
-if ($domain.Count -eq 3)
-{
-   $Servers=Get-ADComputer -Filter * -SearchBase "ou=Servers,dc=$($domain[0]),dc=$($domain[1]),dc=$($domain[2])" -Properties Name, DistinguishedName | Select-Object -Property Name,DistinguishedName 
-}
+$Servers=Get-ADComputer -Filter * -SearchBase "ou=Servers,$DOMAIN" -Properties Name, DistinguishedName | Select-Object -Property Name,DistinguishedName 
 
+#timestamp cannot contain decimal in the format, as that will appear as a file name extension, and prevent export from being created
+$timestamp=(Get-Date -UFormat "%Y%m%d_%H-%M-%S")
 
 
-$AdminExport="C:\Temp\Local Group Membership\LocalAdminMembership.csv"
-$RDP_UsersExport="C:\Temp\Local Group Membership\RemoteDesktop.csv"
+#Define the export paths, including $timestamp in the file name, for records keeping. Also allows you to run query multiple times in the same directory, and not have duplicate filename
+$AdminExport="C:\Temp\Local Group Membership\LocalAdminMembership$timestamp.csv"
+$RDP_UsersExport="C:\Temp\Local Group Membership\RemoteDesktop$timestamp.csv"
 
-
+#create csv header column names, using the Add-Content cmdlet, and defining the $AdminExport file to update
 Add-Content $AdminExport -Value "Server-User"
 
+#Check that WMI port is accesible on the target host, if so, perform query. This does add a little extra time, but worth it for accuracy, and a clean report.
+#You will see errors for hosts that are not reachable on port 135 (WMI), but that will not affect your results.
 foreach ($Server in $Servers)
 {
     if (Test-NetConnection -Port 135  -ComputerName $Server.Name)
@@ -61,9 +64,11 @@ foreach ($Server in $Servers)
     }
 }
 
-
+#create csv header column names, using the Add-Content cmdlet, and defining the $RDP_UsersExport file to update
 Add-Content $RDP_UsersExport -Value "Server-User"
 
+#Check that WMI port is accesible on the target host, if so, perform query. This does add a little extra time, but worth it for accuracy, and a clean report.
+#You will see errors for hosts that are not reachable on port 135 (WMI), but that will not affect your results.
 foreach ($Server in $Servers)
 {
     if (Test-NetConnection -Port 135 -ComputerName $Server.Name)
